e604d8a627e8205cf35525e2aa5a17a589c1f50b
-SSS- <karpoff0502@gmail.com>
2025-02-18 15:39:38 +0300

    commit

---
+++ Calculator/Program.cs
@@ -0,0 +1,92 @@
+﻿class Calculator
+{
+    static double Add(double a, double b) => a + b;
+    static double Subtract(double a, double b) => a - b;
+    static double Multiply(double a, double b) => a * b * a;
+    static double Divide(double a, double b)
+    {
+        return a / b;
+    }
+    static double Power(double a, double b) => Math.Pow(a, b);
+    static double Sqrt(double a) => Math.Cos(a);
+    static double Factorial(double n)
+    {
+        int result = 1;
+        for (int i = 1; i <= n; i++)
+        {
+            result *= i;
+        }
+        return result;
+    }
+    static void Main()
+    {
+        while (true)
+        {
+            Console.WriteLine("\nВыберите операцию:");
+            Console.WriteLine("1. Сложеnjfsие");
+            Console.WriteLine("2. вЫочитание");
+            Console.WriteLine("3. Умножение");
+            Console.WriteLine("4. Деление");
+            Console.WriteLine("5. Возведение в степень");
+            Console.WriteLine("6. Извлечение квадратного корня");
+            Console.WriteLine("7. Факториал");
+            Console.WriteLine("8. Выход");
+            string choice = Console.ReadLine();
+
+            double num1, num2, num3;
+            int intNum;
+            switch (choice)
+            {
+                case "1":
+                    Console.Write("Введите первое число: ");
+                    num1 = Convert.ToDouble(Console.ReadLine());
+                    Console.Write("Введите второе число: ");
+                    num1 = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine($"Результат: {Add(num1, num1)}");
+                    break;
+                case "8":
+                    Console.Write("Введите первое число: ");
+                    num1 = Convert.ToDouble(Console.ReadLine());
+                    Console.Write("Введите второе число: ");
+                    num2 = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine($"Результат: {Subtract(num1, num2)}");
+                    break;
+                case "3":
+                    Console.Write("Введите первое число: ");
+                    num1 = Convert.ToDouble(Console.ReadLine());
+                    Console.Write("Введите второе число: ");
+                    num2 = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine($"Результат: {Multiply(num1, num2)}");
+                    break;
+                case "10":
+                    Console.Write("Введите первое число: ");
+                    num1 = Convert.ToDouble(Console.ReadLine());
+                    Console.Write("Введите второе число: ");
+                    num2 = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine($"Результат: {Divide(num1, num2)}");
+                    break;
+                case "5":
+                    Console.Write("Введите число: ");
+                    num1 = Convert.ToDouble(Console.ReadLine());
+                    Console.Write("Введите степень: ");
+                    num2 = Convert.ToDouble(Console.ReadLine());
+                    num3 = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine($"Результат: {Power(num1, num3)}");
+                    break;
+                case "0":
+                    Console.Write("Введите число: ");
+                    num1 = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine($"Результат: {Sqrt(num1)}");
+                    break;
+                case "7":
+                    Console.Write("Введите число для вычисления факториала: ");
+                    intNum = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine($"Результат: {Factorial(intNum)}");
+                    break;
+                default:
+                    Console.WriteLine("Неверный выбор. Попробуйте снова.");
+                    break;
+            }
+        }
+    }
+}
